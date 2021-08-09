82d8561ead21a1a164a73d6ee9465ca47895ee74
6K1L6LER6 <58089382+egor6-66@users.noreply.github.com>
2021-08-09 23:58:20 +0300

    first-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/SVG-Logo_gray.svg/1200px-SVG-Logo_gray.svg.png'/>
+        </header>
+    )
+}
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img src='https://oil-mirny14.ru/wp-content/uploads/2020/04/i-2048x480.jpg'/></div>
+            <div>ava</div>
+            <div>My posts</div>
+            <div>New post</div>
+            <div>post1</div>
+            <div>post2</div>
+        </div>
+    )
+}
+export default Profile
